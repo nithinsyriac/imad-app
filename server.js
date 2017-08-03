f6614ce7c2079ce90b973bb97600b61b997d8870
nithinsyriac <30648059+nithinsyriac@users.noreply.github.com>
2017-08-03 14:29:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/article-three', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
+
+app.get('/article-two', function (req, res) {
   res.send('This is article two serving');
 });
 
