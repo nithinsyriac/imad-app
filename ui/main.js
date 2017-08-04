f235cd256d9d00efd3d26233363219637d9f6cfd
nithinsyriac <30648059+nithinsyriac@users.noreply.github.com>
2017-08-04 16:32:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,14 @@ console.log('Loaded!');
 //move image
 
 var element=document.getElementById('madi');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 madi.onclick=function()
 {
     madi.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
 };
