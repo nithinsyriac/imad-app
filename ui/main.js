595fd3a4cce9b890029733ec2d0ea151b4f256b1
nithinsyriac <30648059+nithinsyriac@users.noreply.github.com>
2017-08-04 16:40:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element=document.getElementById('madi');
 var marginLeft=0;
 function moveRight()
 {
-    marginLeft=marginLeft+100;
+    marginLeft=marginLeft+20;
     madi.style.marginLeft=marginLeft+'px';
 }
 madi.onclick=function()
