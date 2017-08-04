e0e8015109523e8d52542c9225623cc39df50d38
nithinsyriac <30648059+nithinsyriac@users.noreply.github.com>
2017-08-04 16:24:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 //move image
 
-var element=document.getElementbyId('madi');
+var element=document.getElementById('madi');
 madi.onclick=function()
 {
     madi.style.marginLeft='100px';
