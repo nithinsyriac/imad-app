5af956e7e526ff4ef5f1cd1caba31f7240f5967c
nithinsyriac <30648059+nithinsyriac@users.noreply.github.com>
2017-08-04 16:06:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.get('/article-three', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 
 
 app.get('/ui/madi.png', function (req, res) {
